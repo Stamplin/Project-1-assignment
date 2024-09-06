3bf7c3aa83203462535ef73c481e4b2c21378ffd
Stamplin <701017313@gotvdsb.ca>
2024-09-05 19:39:31 -0700

    Program.cs

--- Project 1 assignment/Program.cs
+++ Project 1 assignment/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Project_1_assignment
+namespace Topic_1___The_Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("                _
+            ,.-" "-.,
+           /   ===   \
+          /  =======  \
+       __|  (o)   (0)  |__      
+      / _|    .---.    |_ \         
+     | /.----/ O O \----.\ |       
+      \/     |     |     \/        
+      |                   |            
+      |                   |           
+      |                   |          
+      _\   -.,_____,.-   /_         
+  ,.-"  "-.,_________,.-"  "-.,
+ /          |       |          \  
+|           l.     .l           | 
+|            |     |            |
+l.           |     |           .l             
+ |           l.   .l           | \,     
+ l.           |   |           .l   \,    
+  |           |   |           |      \,  
+  l.          |   |          .l        |
+   |          |   |          |         |
+   |          |---|          |         |
+   |          |   |          |         |
+   /"-.,__,.-"\   /"-.,__,.-"\"-.,_,.-"\
+  |            \ /            |         |
+  |             |             |         |
+   \__|__|__|__/ \__|__|__|__/ \_|__|__/");
+            Console.WriteLine("Made by Monkey For Monki");
+            Console.WriteLine("");
+            Console.Write("Press any key silly 🦍");
+            Console.ReadLine();
         }
     }
 }
