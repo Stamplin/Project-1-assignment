0b83fc58b114f77abb36e7285c78d0a1ce1e4ed0
Stamplin <701017313@gotvdsb.ca>
2024-09-11 12:45:12 -0700

    Program.cs

--- Project 1 assignment/Program.cs
+++ Project 1 assignment/Program.cs
@@ -4,7 +4,13 @@ namespace Project_1_assignment
     {
         static void Main(string[] args)
         {
-            //This is my initial written in a 5x7 grid following the format in the instructions posted on the assignment.
+            // Printing my name
+            Console.WriteLine("Hello! My name is Stamplin.");
+            Console.WriteLine();
+            // Explaining comments
+            Console.WriteLine("Comments are used to explain the code like make it more readable and help others understand what the code does also they are ignored when the program run.");
+            Console.WriteLine();
+            // This is my initial written in a 5x7 grid following the format in the instructions posted on the assignment.
             Console.WriteLine(" SSSS    K   K ");
             Console.WriteLine("S        K  K  ");
             Console.WriteLine("S        K K   ");
@@ -12,8 +18,6 @@ namespace Project_1_assignment
             Console.WriteLine("     S   K K   ");
             Console.WriteLine("S    S   K  K  ");
             Console.WriteLine(" SSSS    K   K ");
-
-            //I used write line and did it manually, as it made it more challenging than if I were to use an ASCII text art generator and use only one write line and kept on using "/n" to make it look like a grid.
         }
     }
 }
